62cef0c70f1ea2d5c4ec33ef20e803d3faed6e81
ganjali <gaddam.anjali@oracle.com>
2019-06-04 18:45:19 +0530

    Tests for Rest controller methods

--- springapp/src/test/java/com/notekeepingapp/NoteKeepingAppDemo/controller/NoteControllerTest.java
+++ springapp/src/test/java/com/notekeepingapp/NoteKeepingAppDemo/controller/NoteControllerTest.java
@@ -21,6 +21,7 @@ import java.util.Date;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
@@ -40,33 +41,33 @@ public class NoteControllerTest {
     @Test
     public void should_ReturnSuccess_For_getNotes() throws Exception {
         Mockito.when(noteService.getNotes(Mockito.anyString())).thenReturn(mockNotesList);
-        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.get("/list-notes/Anju").accept(MediaType.APPLICATION_JSON)).andExpect(status().isOk()).andReturn();
+        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.get("/Anju/notes").accept(MediaType.APPLICATION_JSON)).andExpect(status().isOk()).andReturn();
         assertEquals(true, result.getResponse().getContentAsString().contains("Anju"));
     }
 
     @Test
     public void should_ReturnSuccess_For_addNote() throws Exception {
         String mockeNoteString = ow.writeValueAsString(mockNote);
-        Mockito.when(noteService.addNote(Mockito.any(Note.class))).thenReturn(true);
-        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.post("/list-notes/Anju").accept(MediaType.APPLICATION_JSON).content(mockeNoteString)
+        Mockito.when(noteService.addNote(Mockito.any(Note.class))).thenReturn(mockNote);
+        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.post("/Anju/notes").accept(MediaType.APPLICATION_JSON).content(mockeNoteString)
                 .contentType(MediaType.APPLICATION_JSON)).andExpect(status().isOk()).andReturn();
-        assertEquals("true", result.getResponse().getContentAsString());
+        assertTrue(result.getResponse().getContentAsString().contains("Anju"));
     }
 
     @Test
     public void should_ReturnSuccess_For_updateNote() throws Exception {
         String mockeNoteString = ow.writeValueAsString(mockNote);
         Mockito.when(noteService.updateNote(Mockito.any(Note.class))).thenReturn(true);
-        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.post("/list-notes/Anju/1").accept(MediaType.APPLICATION_JSON).content(mockeNoteString)
+        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.post("/Anju/notes/1").accept(MediaType.APPLICATION_JSON).content(mockeNoteString)
                 .contentType(MediaType.APPLICATION_JSON)).andExpect(status().isOk()).andReturn();
         assertEquals("true", result.getResponse().getContentAsString());
     }
 
 //    @Test
 //    public void shouldTestDeleteNotesResponseStatusToBeSuccess() throws Exception {
-//        Mockito.when(noteService.addNote(Mockito.any(Note.class))).thenReturn(true);
+//        Mockito.when(noteService.addNote(Mockito.any(Note.class))).thenReturn(mockNote);
 //        Mockito.when(noteService.deleteNote(Mockito.anyInt())).thenReturn(true);
-//        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.delete("/list-notes/Anju/",Mockito.anyInt())).andReturn();
+//        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.delete("/Anju/notes/",1)).andReturn();
 //        assertEquals(200, result.getResponse().getStatus());
 //    }
 }
