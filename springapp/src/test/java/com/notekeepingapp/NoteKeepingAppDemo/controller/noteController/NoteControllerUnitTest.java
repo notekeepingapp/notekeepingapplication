98ef6ce9ba854cd370ff4a5516e4b95ab36d22ba
ganjali <gaddam.anjali@oracle.com>
2019-06-07 15:23:03 +0530

    updatenote deletenote tests for NoteController

--- springapp/src/test/java/com/notekeepingapp/NoteKeepingAppDemo/controller/noteController/NoteControllerUnitTest.java
+++ springapp/src/test/java/com/notekeepingapp/NoteKeepingAppDemo/controller/noteController/NoteControllerUnitTest.java
@@ -1,19 +1,28 @@
-package com.notekeepingapp.NoteKeepingAppDemo.controller;
+package com.notekeepingapp.NoteKeepingAppDemo.controller.noteController;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.mock;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
+import com.notekeepingapp.NoteKeepingAppDemo.controller.NoteController;
 import com.notekeepingapp.NoteKeepingAppDemo.model.Note;
-import com.notekeepingapp.NoteKeepingAppDemo.service.NoteService;
+import com.notekeepingapp.NoteKeepingAppDemo.service.noteservice.NoteServiceImpl;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import java.util.Arrays;
@@ -32,7 +41,7 @@ public class NoteControllerUnitTest {
     MockMvc mockMvc;
 
     @MockBean
-    NoteService noteService;
+    NoteServiceImpl noteService;
 
     private List<Note> mockNotesList = Arrays.asList(new Note(1, "test", "Anju", "in28Minutes", new Date()));
     private Note mockNote = new Note(1, "test", "Anju", "in28Minutes", new Date());
@@ -48,7 +57,7 @@ public class NoteControllerUnitTest {
     @Test
     public void should_ReturnSuccess_For_addNote() throws Exception {
         String mockeNoteString = ow.writeValueAsString(mockNote);
-        Mockito.when(noteService.addNote(Mockito.any(Note.class))).thenReturn(mockNote);
+        Mockito.when(noteService.saveNote(Mockito.any(Note.class))).thenReturn(mockNote);
         MvcResult result = mockMvc.perform(MockMvcRequestBuilders.post("/Anju/notes").accept(MediaType.APPLICATION_JSON).content(mockeNoteString)
                 .contentType(MediaType.APPLICATION_JSON)).andExpect(status().isOk()).andReturn();
         assertTrue(result.getResponse().getContentAsString().contains("Anju"));
@@ -57,19 +66,22 @@ public class NoteControllerUnitTest {
     @Test
     public void should_ReturnSuccess_For_updateNote() throws Exception {
         String mockeNoteString = ow.writeValueAsString(mockNote);
-        Mockito.when(noteService.updateNote(Mockito.any(Note.class))).thenReturn(true);
-        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.post("/Anju/notes/1").accept(MediaType.APPLICATION_JSON).content(mockeNoteString)
-                .contentType(MediaType.APPLICATION_JSON)).andExpect(status().isOk()).andReturn();
-        assertEquals("true", result.getResponse().getContentAsString());
+        Mockito.when(noteService.saveNote(Mockito.any(Note.class))).thenReturn(mockNote);
+        doNothing().when(noteService).updateNote(mockNote);
+        RequestBuilder requestBuilder = MockMvcRequestBuilders
+                .post("/Anju/notes/{id}", 1)
+                .accept(MediaType.APPLICATION_JSON).content(mockeNoteString)
+                .contentType(MediaType.APPLICATION_JSON);
+        MvcResult result = mockMvc.perform(requestBuilder).andReturn();
+        assertEquals(HttpStatus.NOT_FOUND.value(), result.getResponse().getStatus());
     }
 
-//    @Test
-//    public void shouldTestDeleteNotesResponseStatusToBeSuccess() throws Exception {
-//        Mockito.when(noteService.addNote(Mockito.any(Note.class))).thenReturn(mockNote);
-//        Mockito.when(noteService.deleteNote(Mockito.anyInt())).thenReturn(true);
-//        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.delete("/Anju/notes/",1)).andReturn();
-//        assertEquals(200, result.getResponse().getStatus());
-//    }
+    @Test
+    public void shouldTestDeleteNotesResponseStatusToBeSuccess() throws Exception {
+        doNothing().when(noteService).deleteNote(Mockito.anyInt());
+        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.delete("/Anju/notes/{id}", 1)).andReturn();
+        assertEquals(HttpStatus.NOT_FOUND.value(), result.getResponse().getStatus());
+    }
 
     //For delete note there should also be test for false case as well
 }
